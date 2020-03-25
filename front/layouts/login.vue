b3260a54064ec8d208fb2498898f356af71e0fcb
朱娟娟 <zhjnjn93@163.com>
2020-03-25 16:54:21 +0800

    注册功能实现

--- front/layouts/login.vue
+++ front/layouts/login.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div>导航</div>
+    <!-- <div>导航</div> -->
     <nuxt></nuxt>
-    <div>底部信息</div>
+    <!-- <div>底部信息</div> -->
   </div>
 </template>
 
@@ -11,6 +11,26 @@ export default {
     
 }
 </script>
-<style lang="stylus" scoped>
-
+<style lang="stylus" >
+  .login-container
+    width 100%
+    height  100%
+    .login-form
+      width 550px
+      padding 150px 0
+      margin 0 auto 
+      overflow hidden
+      .title-container
+        text-align center
+        margin-bottom 20px
+        img
+          width 200px
+      .captcha-container
+        width 400px
+        .captcha
+          position absolute
+          right -150px
+          img
+            width 120px
+            height 40px    
 </style>
---
+++ front/pages/register.vue
@@ -0,0 +1,122 @@
+
+<template>
+  <div class="login-container">  
+    <el-form class="login-form" label-width="100px" :model="form" :rules="rules" ref="registerForm">
+       
+      <div class="title-container">
+        <img src="/logo.png" alt />
+      </div>
+
+      <el-form-item props="email" label="邮箱：">       
+        <el-input v-model="form.email" placeholder="请输入邮箱"></el-input>
+      </el-form-item>
+
+       <el-form-item props="nickName" label="昵称：">       
+        <el-input v-model="form.nickName" placeholder="请输入昵称"></el-input>
+      </el-form-item>
+
+      <el-form-item props="captcha" label="验证码：" class="captcha-container">
+          <div class="captcha">
+            <img :src="code.captcha" @click="resetCaptcha">
+        </div>
+        <el-input v-model="form.captcha"  placeholder="请输入验证码"></el-input>
+      </el-form-item>
+
+      <el-form-item props="pwd" label="密码：">       
+        <el-input v-model="form.pwd" placeholder="请输入密码"></el-input>
+      </el-form-item>
+
+      <el-form-item props="repwd" label="确认密码：">       
+        <el-input v-model="form.repwd" placeholder="请再次输入密码"></el-input>
+      </el-form-item>
+
+        <el-form-item >
+            <el-button type="primary" @click.native.prevent="handleRegister">注册</el-button> 
+        </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+import md5 from 'md5';
+export default {
+  layout: "login",
+  data() {
+    return {
+      form: {
+        email: "",
+        nickName:"",
+        pwd: "",
+        repwd:"",  
+        captcha:""   
+      },
+      rules: {
+        email: [
+          { required: true, message: "请输入邮箱" },
+          { type: 'email', message: "请输入正确的邮箱格式" }
+        ],
+        nickName:[
+            {required: true, message: "请输入昵称"}
+        ],
+        pwd:[
+            {required: true, pattern:/^[\w_-]{6,12}/g, message: "请输入6~12位密码"}
+        ],
+        repwd:[
+            {required: true, message: "请再次输入密码"},
+            { validator:(rule,value,callback)=>{
+                if(value!==this.form.pwd){
+                    callback( new Error('两次密码不一致'))
+                }
+                callback()
+            }}
+        ]
+      },
+      code:{
+          captcha:"/api/captcha"
+      }
+    };
+  },
+  methods: {
+      resetCaptcha(){
+          this.code.captcha = '/api/captcha?_t'+new Date().getTime()
+      },
+      handleRegister(){
+          this.$refs.registerForm.validate( async vaild=>{
+              if(vaild){
+                  console.log("校验成功")
+                //   @todo 发送注册请求
+                let obj = {
+                    email:this.form.email,
+                    nickName:this.form.nickName,
+                    pwd:md5(this.form.pwd),
+                    captcha:this.form.captcha
+                }
+                let ret = await this.$http.post('urser/register',obj)
+                // code=0 就是成功
+                if(ret.code==0){
+                    this.$alert('注册成功',"成功",{
+                        confirmButtonText:"去登录",
+                        callback:()=>{
+                            this.$router.push('/login')
+                        }
+                    })
+                }else{
+                    this.$message.error(ret.message)
+                }
+              }else{
+                  console.log("校验失败")
+              }
+          })
+      }
+  }
+ 
+};
+</script>
+
+
+<style lang="scss">
+.login-form {
+  width: 600px;
+  margin: 0 auto;
+}
+</style>
