fd9f16e46c58e60390ab3f0716b8b63914397be7
朱娟娟 <zhjnjn93@163.com>
2020-03-26 18:39:02 +0800

    注册路由配置

---
+++ serve/app/controller/user.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-empty-function */
+const BaseController = require('./base');
+
+class UserController extends BaseController {
+  async login() {
+
+  }
+
+  async regisiter() {
+
+  }
+
+  async verify() {
+    // 校验用户名是否存在
+  }
+
+  async info() {
+
+  }
+}
